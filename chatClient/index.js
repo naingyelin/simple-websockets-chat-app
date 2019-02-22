07ca10ccc0fe23f8742a5d662c05080bebf14869
Naing Thein <theinn@snowyhydro.com.au>
2019-02-22 15:53:57 +1100

    using sockette and vanilla websocket

--- chatClient/index.js
+++ chatClient/index.js
@@ -2,8 +2,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
 app.use(express.static('html'));
-// app.use(express.static('css'));
-// app.use(express.static('js'));
-app.get('/api', (req, res) => res.send('Hello World!'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/html/index.html');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
